8f8a6ec2c1476b837f2ab0cf274f4d259a10275d
Reyansh p <reyansh.pradeep@gmail.com>
2021-12-30 16:29:01 +0530

    practice commit

--- First file.py
+++ First file.py
@@ -1,2 +1,2 @@
 print("This is the first file. I suggest you create a similar file and we just keep it in this for no reason lmaoooooooooo")
-print("damnnn this is sooo cool always wanted to do sum with GIT")
+print("this is for my practice and im playing around with GIT  to learn :) ")
