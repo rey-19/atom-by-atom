cf96905fedd247651282300d53dca92daa50e364
Ave-S <sharpphoenixlit@gmail.com>
2021-12-31 19:42:51 +0530

    test

--- First file.py
+++ First file.py
@@ -1,2 +1,4 @@
 print("This is the first file. I suggest you create a similar file and we just keep it in this for no reason lmaoooooooooo")
 print("this is for my practice and im playing around with GIT  to learn :) ")
+
+## Bruh, check disc
